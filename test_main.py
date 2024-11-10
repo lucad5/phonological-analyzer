f8df1b29cee1dc33b733ae0654dda300ca476789
lucad5 <87230523+lucad5@users.noreply.github.com>
2024-11-09 20:07:12 -0400

    Add placeholder pseudocode and pytest tests

---
+++ test_main.py
@@ -0,0 +1,34 @@
+import pytest
+import os
+import sys
+
+import main
+
+# TOOD: fill in these tests
+def test_existing_file_is_opened_correctly():
+    placeholder_variable = 1
+    assert placeholder == 2
+
+def test_nonexistant_file_is_handled_correctly():
+    placeholder_variable = 1
+    assert placeholder == 2
+
+def test_list_of_unique_environments_contains_correct_environments():
+    placeholder_variable = 1
+    assert placeholder == 2
+
+def test_spreadsheet_generated_exists():
+    placeholder_variable = 1
+    assert placeholder == 2
+
+def test_spreadsheet_generated_is_the_correct_file_format():
+    placeholder_variable = 1
+    assert placeholder == 2
+
+def test_spreadsheet_generated_is_correct():
+    placeholder_variable = 1
+    assert placeholder == 2
+
+def test_spreadsheet_generated_is_correct():
+    placeholder_variable = 1
+    assert placeholder == 2
